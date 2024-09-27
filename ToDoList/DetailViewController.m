2363c3026b2e791158d366c464eb28c2a4b8c94b
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-09-27 13:27:20 +0300

    Implementation of hiding keyboard

--- ToDoList/DetailViewController.m
+++ ToDoList/DetailViewController.m
@@ -7,7 +7,7 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
+@interface DetailViewController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *titleTextField;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 @property (weak, nonatomic) IBOutlet UIButton *saveButton;
@@ -20,10 +20,25 @@
     [super viewDidLoad];
     
     [self.saveButton addTarget:self action:@selector(save) forControlEvents:UIControlEventTouchUpInside];
+    
+    UITapGestureRecognizer *handleTap =[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleEndEditing)];
+    [self.view addGestureRecognizer:handleTap];
 }
 
-- (void) save {
+- (void) handleEndEditing{
+    [self.view endEditing:YES];
+}
+
+- (void)save {
     NSLog(@"Save");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if ([textField isEqual:self.titleTextField]) {
+        [self.titleTextField resignFirstResponder];
+    }
+    
+    return YES;
 }
 
 @end
